90cd165e661f6ce94f5482c8ae4a48e5bd21ccba
garrick <difeng.ng@grabtaxi.com>
2023-09-29 00:37:36 +0800

    Init

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+const helmetContext = {};
+
+const App: React.FC<{}> = () => {
+  const [theme, setTheme] = useState("dark");
+
+  return (
+    <>
+      <HelmetProvider context={helmetContext}>
+        {/* Default SEO  */}
+        <Helmet>
+          <meta
+            name="description"
+            content="Full stack engineer with 4 years of experience in startup, fintech and broadcasting industry. Pioneer engineer in
+      building a startup app - Bjak.my that serves more than millions of customers in Malaysia. Leader in setting up an
+      RPA team that automates 30+business processes in Astro. Self educated developer with promising learning ability."
+          />
+
+          <meta name="keywords" content="programming,coding,nodejs,typescript" />
+          <meta name="designer" content="ndf_724@hotmail.com, Garrick" />
+          <meta name="author" content="ndf_724@hotmail.com, Garrick" />
+          <meta name="robots" content="index, follow" />
+          <meta name="revisit-after" content="7 days" />
+          <meta
+            property="og:image"
+            content="https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1200&q=630"
+          />
+        </Helmet>
+      </HelmetProvider>
+    </>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "index.[contenthash].bundle.js",
+    assetModuleFilename: "images/[hash][ext][query]",
+    clean: true,
+  },
+  mode: process.env.NODE_ENV || "development",
+  resolve: {
+    alias: {
+      "@src": path.resolve(__dirname, "./src/"),
+    },
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: { contentBase: path.join(__dirname, "src"), port: 3001, historyApiFallback: true },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "postcss-loader", // post process the compiled CSS
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "sitemap.xml", to: "sitemap.xml", force: true },
+        { from: "robots.txt", to: "robots.txt", force: true },
+      ],
+      options: {
+        concurrency: 100,
+      },
+    }),
+  ],
+};
